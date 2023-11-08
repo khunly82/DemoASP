7afa4f01f3e15e4a9ea989fcd9deba88d2fee669
khun <lykhun@gmail.com>
2023-11-08 17:50:49 +0100

    Add Product Controller

---
+++ DemoASP/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Id = 42, Name = "Coca Cola" });
+        }
+    }
+}
